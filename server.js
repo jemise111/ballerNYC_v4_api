a2cecd9449d5996fb19f5a31d441b5ec3dfcaaa5
Jesse Sessler <jesse.sessler@gmail.com>
2015-11-14 20:01:19 -0500

    cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var cors = require('cors');
 var config = require('./config');
 var db = require('./db/db');
 var formatter = require('./helpers/dataFormatter');
@@ -7,14 +8,9 @@ var formatter = require('./helpers/dataFormatter');
 
 var app = express();
 var port = process.env.PORT || config.port;
+app.use(cors());
 
 // routes
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 app.get('/courts', function(req, res){
   var options = formatter.formatQueryIntoOptions(req.query);
